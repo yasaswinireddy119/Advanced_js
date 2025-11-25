19e467add34456e7e88307e32836ee95660175d8
yasaswinireddy119 <yasaswinireddykotha@gmail.com>
2025-11-25 07:19:50 +0530

    closures

---
+++ closures/global_fun_execution.js
@@ -0,0 +1,16 @@
+<script>
+var age = 10;
+
+
+function displayAge(age){
+    console.log("displayAge",age)
+}
+
+function changeAge(age){
+    age++
+    console.log("changeAge",age)
+}
+
+displayAge(age)
+changeAge(age)
+</script>
